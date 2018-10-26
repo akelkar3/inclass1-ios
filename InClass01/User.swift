2767efc465fde5fce1df637cb3d4beb0481611fc
Ankit Kelkar <akelkar3@uncc.edu>
2018-10-25 23:18:25 -0400

    adding the entire project

---
+++ InClass01/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  InClass01
+//
+//  Created by Ankit Kelkar on 10/17/18.
+//  Copyright © 2018 Ankit Kelkar. All rights reserved.
+//
+
+import Foundation
+
+class User{
+  var  name:String = ""
+  var  dep:String = ""
+    init(name:String,dep:String){
+    self.name = name
+    self.dep = dep
+    }
+}
---
+++ InClass01Tests/InClass01Tests.swift
@@ -0,0 +1,34 @@
+//
+//  InClass01Tests.swift
+//  InClass01Tests
+//
+//  Created by Ankit Kelkar on 10/17/18.
+//  Copyright © 2018 Ankit Kelkar. All rights reserved.
+//
+
+import XCTest
+@testable import InClass01
+
+class InClass01Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
